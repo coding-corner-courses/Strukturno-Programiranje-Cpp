05ba7b422dd05e4c6fdda1a74d5a2523d659bd3e
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-10-06 00:06:31 +0200

    Create zadaca3.cpp

---
+++ Tema1/Zadaci-od-predavanje/zadaca3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+//
+//Напиши програма преку која ќе можеш да внесеш два броеви од тастура и да го најдеш коренот од збирот на нивните квадрати.
+//Резултатот отпечати го на екран.
+int main()
+{
+    int number1, number2;
+
+    cin>>number1>>number2;
+
+    float result = sqrt(pow(number1,2)+pow(number2,2));
+
+    cout<<"The result is: "<<result;
+
+    return 0;
+}
