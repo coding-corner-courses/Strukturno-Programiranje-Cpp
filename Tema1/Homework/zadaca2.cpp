6cec5ed80a879c03a639d0ff8d10a39dfde30332
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-10-07 23:16:52 +0200

    Create zadaca2.cpp

---
+++ Tema1/Homework/zadaca2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//Напиши програма каде што преку тастатура ќе може да се внесат два цели броеви.
+//Пронајди го нивниот производ и провери дали нивниот производ е делив со бројот 7 или со бројот 13.
+//Резултатот отпечати го на екран.
+int main()
+{
+    int number1, number2;
+
+    cin>>number1>>number2;
+
+    int result = number1 * number2;
+
+
+    // Бројот е делив со 7 или со 13.
+    bool condition = result % 7 == 0 || result % 13 == 0;
+
+    cout<<"The result is divisible with 7 or 13: "<<condition<<endl;
+
+    return 0;
+}
