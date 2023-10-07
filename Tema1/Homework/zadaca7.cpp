e75a2e1f2a5c8ea5bf8dd9567cdeab5503325011
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-10-07 23:58:32 +0200

    Create zadaca7.cpp

---
+++ Tema1/Homework/zadaca7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//Задача 7
+//Напиши програма каде што преку тастатура ќе може да се внесат два броеви (b, h).
+//Пронајди ја плоштината (A) според наведената формула и отпечати го резултатот на екран.
+int main()
+{
+    int base, height;
+
+    cin>>base>>height;
+
+    int area=base*height;
+
+    cout<<"The result is: "<<area<<endl;
+
+    return 0;
+}
