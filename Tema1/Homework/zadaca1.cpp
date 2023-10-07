cee07e3457ef0d034382ea143fc58999858228e3
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-10-07 23:05:13 +0200

    Create zadaca1.cpp

---
+++ Tema1/Homework/zadaca1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//Напиши програма каде што преку тастатура ќе може да се внесат два цели броеви.
+//Пронајди го збирот на броевите и провери дали збирот е парен број. 
+//Резултатот отпечати го на екран.
+int main()
+{
+    int number1, number2;
+
+    cin>>number1>>number2;
+
+    int result = number1 + number2;
+
+    bool isEvenNumber = result % 2 == 0;
+
+    cout<<"The result is even number: "<<isEvenNumber<<endl;
+
+    return 0;
+}
