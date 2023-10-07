e0d2c645fab2b7d660c96cc40c4bc4ed7b4fd6be
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-10-07 23:47:08 +0200

    Create zadaca4.cpp

---
+++ Tema1/Homework/zadaca4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//Напиши програма каде што преку тастатура ќе може да се внесат три броеви (b,h,l), 
+//пронајди го волуменот(V) според наведената формула и отпечати го резултатот на екран.
+int main()
+{
+    int base, height, legth;
+
+    cin>>base>>height>>length;
+
+    float volume=base*height*length/2.0;
+
+    cout<<"The result is: "<<volume<<endl;
+
+    return 0;
+}
