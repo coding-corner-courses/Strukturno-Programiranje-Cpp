753767629ac9b2b04c6489be402e18ed8eba1b4b
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-10-11 00:45:01 +0200

    Create zadaca2.cpp

---
+++ Tema3/Homework/zadaca2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+//Задача 2
+//Напиши програма каде преку тастатура ќе може да се внеси цел број. 
+//Програмата треба да провери дали бројот е делив со 61 
+//и соодветно да отпечати “Brojot __ e deliv so 61” или “Brojot __ ne e deliv so 61”.
+int main()
+{
+    int number;
+
+    cin>>number;
+
+    if(number%10==61)
+    {
+        cout<<"Brojot "<<number<<" e deliv so 61"<<endl;
+    }
+    else
+    {
+        cout<<"Brojot "<<number<<"ne e deliv so 61"<<endl;
+    }
+
+    return 0;
+}
