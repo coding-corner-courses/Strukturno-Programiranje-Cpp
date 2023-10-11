96e22cdea5d7597d425749eec2ec73210cc9f353
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-10-12 00:26:11 +0200

    Update zadaca18.cpp

--- Tema3/Homework/zadaca18.cpp
+++ Tema3/Homework/zadaca18.cpp
@@ -3,44 +3,25 @@
 using namespace std;
 
 //Задача 18
-//Напиши програма каде ќе се внеси еден број преку тастатура 
-//и ќе се провери дали цифрата на единици е поголема од 4. Резултатот да се отпечати на екран. 
+//Напиши програма каде ќе се внеси еден број преку тастатура
+//и ќе се провери дали цифрата на единици е поголема од 4. Резултатот да се отпечати на екран.
 int main()
 {
-    int number1, number2, number3;
+    int number;
 
-    cin>>number1>>number2>>number3;
+    cin>>number;
 
-    int minNumber, maxNumber;
+    int onesDigit=number%10;
 
-    if(number1<number2 && number1<number3)
+
+    if(onesDigit>4)
     {
-        minNumber=number1;
-    }
-    else if(number2<number1 && number2<number3)
-    {
-        minNumber=number2;
+        cout<<"Cifrata na edinici e pogolema od 4"<<endl;
     }
     else
     {
-        minNumber=number3;
+        cout<<"Cifrata na edinici e pomala ili ednakva na 4"<<endl;
     }
-
-    if(number1>number2 && number1>number3)
-    {
-        maxNumber=number1;
-    }
-    else if(number2>number1 && number2>number3)
-    {
-        maxNumber=number2;
-    }
-    else
-    {
-        maxNumber=number3;
-    }
-
-
-    cout<<"Result: "<<maxNumber-minNumber;
 
     return 0;
 }
