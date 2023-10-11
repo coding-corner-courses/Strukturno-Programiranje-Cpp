97be4b9899c2dad3fd63949abd4579fde19b0a30
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-10-11 16:08:19 +0200

    Create zadaca11.cpp

---
+++ Tema3/Homework/zadaca11.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//Задача 11
+//Напиши програма каде преку тастатура ќе може да се внесат два цели броеви.
+//Да се отпечати на екран поголемиот од броевите - “Brojot __ e pogolem od brojot __”.
+int main()
+{
+    int number1, number2;
+
+    cin>>number1>>number2;
+
+    int maxNumber;
+
+    if(number1>number2)
+    {
+        cout<<"Brojot "<<number1<<" e pogolem od brojot "<<number2<<endl;
+    }
+    else
+    {
+        cout<<"Brojot "<<number2<<" e pogolem od brojot "<<number1<<endl;
+    }
+
+    return 0;
+}
