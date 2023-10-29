773b854a8f14a04e0e72969c27e0a0e4e348c45c
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-10-29 20:58:08 +0100

    Create zadaca5.cpp

---
+++ Tema3/Exercises-from-lesson/zadaca5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// Задача 5
+//Напишете програма која ќе проверува дали внесен карактер е голема буква. Отпечатете го резултатот на екран.
+//Големите букви во ASCII се во рангот [65,90]
+int main()
+{
+    char character;
+
+    cin>>character;
+
+    int codeCharacter = (int) character;
+
+    if(codeCharacter >=65 && codeCharacter<=90){
+        cout<<"UPPER CASE LETTER"<<endl;
+    }
+
+    return 0;
+}
