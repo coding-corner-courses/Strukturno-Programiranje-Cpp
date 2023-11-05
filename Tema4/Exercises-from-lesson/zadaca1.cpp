a5e63f66c2864a52e3303da7d17240f998c49984
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-11-05 19:57:04 +0100

    Update zadaca1.cpp

--- Tema4/Exercises-from-lesson/zadaca1.cpp
+++ Tema4/Exercises-from-lesson/zadaca1.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+//Напиши програма каде ќе се испечатат сите броеви од 1-100.
+//Резултатот да се отпечати.
+int main()
+{
+
+    for(int i=0; i<100; i++){
+        cout<<i+1<<endl;
+    }
+
+    return 0;
+}
