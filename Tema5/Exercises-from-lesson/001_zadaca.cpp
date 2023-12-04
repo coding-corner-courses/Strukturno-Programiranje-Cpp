97f290091a2de7b36d151efb78cc893adb589fce
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-12-04 23:48:03 +0100

    Update 001_zadaca.cpp

--- Tema5/Exercises-from-lesson/001_zadaca.cpp
+++ Tema5/Exercises-from-lesson/001_zadaca.cpp
@@ -1,1 +1,32 @@
 
+#include <iostream>
+
+using namespace std;
+
+//Задача 1
+//Напиши програма каде преку стандарден влез се внесува еден цел број - n.
+//Програмата треба да овозможи внесување на n-парови природни броеви и потребно е за секој пар да го отпечати збирот на броевите.
+void findSum(){
+
+    int number1, number2;
+
+    cin>>number1>>number2;
+
+    int result = number1 + number2;
+
+    cout<<"Result: "<<result<<endl;
+}
+
+int main()
+{
+    int n;
+
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+
+        findSum();
+    }
+
+    return 0;
+}
