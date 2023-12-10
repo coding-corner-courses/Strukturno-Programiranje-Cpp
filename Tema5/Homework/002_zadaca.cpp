2fc446726ede34a87aa52406a62cde377a91afe2
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-12-10 11:29:23 +0100

    Update 002_zadaca.cpp

--- Tema5/Homework/002_zadaca.cpp
+++ Tema5/Homework/002_zadaca.cpp
@@ -3,23 +3,25 @@
 using namespace std;
 
 //Задача 2
-//Напиши програма каде за внесен број ќе се провери дали бројот е делив со најголемата цифра која ја содржи. Резултатот да се отпечати. 
+//Напиши програма каде за внесен број ќе се провери дали бројот е делив со најголемата цифра која ја содржи. Резултатот да се отпечати.
 //а. Направи функција која ќе ја враќа најголемата цифра од даден број
 
 int getMaximumDigit(int number)
 {
-    
+
     int maximumDigit = 0;
     int newNumber = number;
-    
-    while(newNumber != 0){
-        
+
+    while(newNumber != 0)
+    {
+
         int digit = newNumber % 10;
-        
-        if(digit>maximumDigit){
+
+        if(digit>maximumDigit)
+        {
             maximumDigit= digit;
         }
-        
+
         newNumber = newNumber / 10;
     }
 
@@ -35,10 +37,13 @@ int main()
     int maxDigit = getMaximumDigit(n);
 
     cout<<"The max digit is: "<<maxDigit<<endl;
-    
-    if(n%maxDigit){
+
+    if(n%maxDigit==0)
+    {
         cout<<"The number is divisible with max digit"<<endl;
-    } else {
+    }
+    else
+    {
         cout<<"The number is not divisible with max digit"<<endl;
     }
 
