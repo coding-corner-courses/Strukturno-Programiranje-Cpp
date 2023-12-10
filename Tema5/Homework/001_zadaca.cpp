df46be368c29dc578d90280686e34a7015f59f64
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-12-10 11:22:28 +0100

    Update 001_zadaca.cpp

--- Tema5/Homework/001_zadaca.cpp
+++ Tema5/Homework/001_zadaca.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
 
+using namespace std;
+
+//Задача 1
+//Напиши функција која е потребно да провери дали даден број е прост (бројот е зададен како аргумент во функцијата).
+//Функцијата треба да врати 0, доколку бројот не е прост и 1, доколку бројот е прост.
+
+int isPrime(int number)
+{
+
+    for(int i=2; i<number; i++)
+    {
+
+        if(number % i == 0)
+        {
+
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main()
+{
+    int n;
+
+    cin>>n;
+
+    int result = isPrime(n);
+
+    cout<<"Is prime: "<<result<<endl;
+
+    return 0;
+}
