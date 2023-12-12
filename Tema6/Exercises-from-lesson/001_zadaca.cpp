5f4a58836608daa8725ddcc3d62fd010cb1f3994
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-12-12 21:46:31 +0100

    Update 001_zadaca.cpp

--- Tema6/Exercises-from-lesson/001_zadaca.cpp
+++ Tema6/Exercises-from-lesson/001_zadaca.cpp
@@ -1,1 +1,27 @@
 
+#include <iostream>
+
+using namespace std;
+
+// Задача1
+// Да се напише програма со која ќе може да се внесат преку тастатура 10 броеви кои ќе бидат запишани во низа. Отпечати ја низата.
+int main()
+{
+    int numbers[10];
+
+    for(int i=0; i<10; i++){
+
+        int number;
+
+        cin>>number;
+
+        numbers[i]=number;
+    }
+
+    for(int i=0; i<10; i++){
+
+        cout<<numbers[i]<<" ";
+    }
+
+    return 0;
+}
