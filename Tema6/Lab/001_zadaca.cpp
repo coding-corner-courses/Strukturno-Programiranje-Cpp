909b1dab94d3f731d88d9699279c79f92ecd29be
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-12-18 20:15:19 +0100

    Create 001_zadaca.cpp

---
+++ Tema6/Lab/001_zadaca.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+
+void insertArray(int newArray[], int n)
+{
+
+    for(int i=0; i<n; i++)
+    {
+
+        int number;
+
+        cin>>number;
+
+        newArray[i]=number;
+    }
+}
+
+
+void printArray(int newArray[], int n)
+{
+
+    for(int i=0; i<n; i++)
+    {
+
+        cout<<newArray[i]<<" ";
+    }
+
+    cout<<endl;
+}
+
+int main()
+{
+    int n, m;
+
+    cin>>n;
+
+    int niza1[n];
+
+    insertArray(niza1, n);
+
+    cin>>m;
+
+    int niza2[m];
+
+    insertArray(niza2, m);
+
+    int resultN = m+n;
+
+    int resultArray[resultN];
+
+    int indexArray1=0;
+    int indexArray2=0;
+
+    for(int i=0; i< resultN ; i++)
+    {
+
+
+        if((indexArray1 < n && indexArray2 >= m) || niza1[indexArray1] <= niza2[indexArray2])
+        {
+
+            resultArray[i]=niza1[indexArray1];
+            indexArray1++;
+        }
+        else if((indexArray1 >= n && indexArray2 < m) || niza1[indexArray1] > niza2[indexArray2])
+        {
+
+            resultArray[i]=niza2[indexArray2];
+            indexArray2++;
+        }
+    }
+
+    printArray(niza1, n);
+    printArray(niza2, m);
+    printArray(resultArray, resultN);
+
+    return 0;
+}
