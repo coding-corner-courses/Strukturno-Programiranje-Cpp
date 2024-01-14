0ba2a9010e6d72ce5369df758a2f39d21c94626f
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2024-01-14 13:19:24 +0100

    Create 000_example.cpp

---
+++ Tema8/Exercises-from-lesson/000_example.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Call by value
+void fun1(int a)
+{
+    a=20;
+}
+
+//Call-By-Reference with a Reference Argument
+void fun2(int &b)
+{
+    b=20;
+}
+
+//Call-By-Reference with a Pointer Argument
+void fun3(int *b)
+{
+    *b=30;
+}
+
+int main()
+
+{
+
+    int var =10;
+
+    fun1(var);
+
+    cout<<"Value of variable var: "<<var<<endl;
+
+    fun2(var);
+
+    cout<<"Value of variable var: "<<var<<endl;
+
+    fun3(&var);
+
+    cout<<"Value of variable var: "<<var<<endl;
+
+    return 0;
+}
