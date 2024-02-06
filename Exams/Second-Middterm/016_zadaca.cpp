3dd2bcd224f8fd043d26992750d947baddfed927
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2024-02-06 23:21:30 +0100

    Create 016_zadaca.cpp

---
+++ Exams/Second-Middterm/016_zadaca.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+//Задача 16
+//Да се напише рекурзивна функција за наоѓање на максималната цифра од даден цел број.
+//
+//Од стандарден влез се внесуваат непознат број цели броеви се додека не се внесе нешто што не е број.
+//За секој од нив да се испечати максималната цифра во посебен ред.
+//
+//Забелешка: Решението со рекурзивна функција носи 100% од поените, а со нерекурзивна функција 70% од поените.
+//
+//Забелешка: ЗАБРАНЕТО е користење на глобални променливи.
+
+int findMax(int number, int maxDigit)
+{
+
+    if(number == 0)
+    {
+        return maxDigit;
+    }
+
+    int digit = number % 10;
+    int newMaxDigit = digit > maxDigit ? digit : maxDigit;
+
+    return findMax(number/10, newMaxDigit);
+}
+
+int main()
+{
+    int number;
+
+    while(cin>>number)
+    {
+        int maxDigit = findMax(number, 0);
+
+        cout<<maxDigit<<endl;
+
+    }
+
+    return 0;
+}
