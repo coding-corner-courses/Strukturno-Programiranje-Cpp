b582e6d8ee97bdef9b0692ae75b256d11ae6d7c4
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2024-06-14 15:35:08 +0200

    Create 201_zadaca.cpp

---
+++ Exams/Second-Middterm/201_zadaca.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include<cstring>
+
+using namespace std;
+
+//Испит Јуни 13, 2024
+//Напиши програма која ќе овозможи внесување на низа од знаци.
+//Програмата е потребно да отпечати кој карактер се појавува најмалку пати во низата и да се отпечати бројот на појавувања.
+//Доколку има повеќе такви карактери да се отпечати првиот карактер кој се појавува во низата.
+//Пр.  Test
+//
+//Излез: T 1
+
+//Пр.  kalabalak
+//
+//Излез: b 1
+
+// Препоставка: Доколку има повеќе карактери кои се појавуваат ист број на пати, да се отпечати првиот карактер кој што се појавува во низата
+// Претпоставка: Мали од големи букви се бројат како различен карактер
+int main()
+{
+    char characters[100];
+
+    cin>>characters;
+
+    int lengthOfArray = strlen(characters);
+
+    char charWithMinOcurrences = characters[0];
+    int minOccurences = lengthOfArray;
+
+    for(int i=0; i<lengthOfArray; i++)
+    {
+        int charOccurences = 0;
+        for(int j=0; j<lengthOfArray; j++)
+        {
+            if(characters[i]==characters[j])
+            {
+                charOccurences++;
+            }
+        }
+
+        if(minOccurences>charOccurences)
+        {
+            minOccurences = charOccurences;
+            charWithMinOcurrences= characters[i];
+        }
+    }
+
+    cout<<charWithMinOcurrences<<" "<<minOccurences<<endl;
+
+
+    return 0;
+}
